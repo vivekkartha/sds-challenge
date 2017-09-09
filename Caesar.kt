1c24d2fa697669f99b698115931f5f89612fd2d9
Vivek Kartha <vivek.kartha@mobme.in>
2017-09-09 15:36:24 +0530

    Adding Caeser.kt

---
+++ Caesar.kt
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+object CaesarCipher {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    val inputText = input.nextLine()
+    println("Enter your key")
+    val key = Integer.parseInt(input.nextLine())
+    if (key in 1..26) {
+      println(cipher(inputText, key))
+    } else
+      println("Key error")
+  }
+
+  private fun cipher(inputText: String, key: Int): String {
+    var outputText = ""
+    for (c in inputText.toCharArray()) {
+      outputText += when {
+        Character.isUpperCase(c) -> (((c - 'A' + key) % 26) + 'A'.toInt()).toChar()
+        Character.isLowerCase(c) -> (((c - 'a' + key) % 26 )+ 'a'.toInt()).toChar()
+        else -> c
+      }
+    }
+    return outputText
+  }
+}
